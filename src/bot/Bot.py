b069adac2f61672ee80626733ab7e1a1522d27c8
Wiktor Sierpinski <vaighir@gmail.com>
2020-11-15 12:35:34 +0100

    Add comments

--- src/bot/Bot.py
+++ src/bot/Bot.py
@@ -8,10 +8,12 @@ import mysql_helper
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
 
+# List of nonsense expressions the bot says
 RANDOM_PARROT_CHATTER = ["SQUAWK!!!", "Squawky wants a cookie!!!", "Squawky is a good bird!",
                          "Let's make discord great again!", "SQUAWK!", "Cookie!", "The cake is a lie!",
                          "I'm Squawky!", "I'm a star"]
 
+# Text displayed when users ask for help
 HELP_TEXT = """`!parrot read` to read messages on this channel
 %s"""
 
@@ -19,6 +21,7 @@ channels = []
 users = []
 
 
+# Returns a nicely formatted string with all users whose messages have been read
 def users_to_text():
     users_as_text = ""
     for u in users:
@@ -29,21 +32,26 @@ def users_to_text():
     return users_as_text
 
 
+# Pick a random thing for the bot to say
 def pick_random_chatter():
     return RANDOM_PARROT_CHATTER[random.randint(0, len(RANDOM_PARROT_CHATTER) - 1)]
 
 
 class MyClient(discord.Client):
+    # Log in
     async def on_ready(self):
         print("I've logged in. Squawk!")
 
+    # React to messages
     async def on_message(self, message):
+        # Ignore own messages
         if message.author == client.user:
             return
 
         if message.content.startswith("!parrot"):
             message_content = message.content.split(" ")
 
+            # Respond with random chatter to message "!parrot"
             if len(message_content) == 1:
                 await message.channel.send(pick_random_chatter())
 
@@ -59,9 +67,11 @@ class MyClient(discord.Client):
                 await message.channel.send("SQUAWK! I don't understand!")
                 await self.show_help(message)
 
+    # Display help message
     async def show_help(self, message):
         await message.channel.send(HELP_TEXT % pick_random_chatter())
 
+    # Read and save messages from a channel
     async def read_channel(self, channel):
         messages = await channel.history(limit=20000).flatten()
         for m in messages:
