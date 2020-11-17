d013ca9351c3a4e6815620a1c6febe815ea33111
Wiktor Sierpinski <vaighir@gmail.com>
2020-11-17 18:51:00 +0100

    Implement learning

--- src/bot/learn_module.py
+++ src/bot/learn_module.py
@@ -5,8 +5,42 @@ import json
 import mysql_helper
 
 
+def learn(dictionary, first_words, message):
+    tokens = message.split(" ")
+
+    if len(tokens) > 1:
+        first_words.append(tokens[0])
+
+    for i in range(0, len(tokens)-1):
+        current = tokens[i]
+
+        next = tokens[i+1]
+
+        if current in dictionary:
+            nextWords = dictionary[current]
+
+            if next in nextWords:
+                dictionary[current][next] = dictionary[current][next] + 1
+            else:
+                dictionary[current][next] = 1
+
+        else:
+            dictionary[current] = {next: 1}
+
+
 def main(author, server):
+    # TODO delete old dictionary from the database
+    dictionary = dict()
+    first_words = []
     messages = mysql_helper.load_messages(author, server)
 
-    for m in messages:
-        print(m)
+    for message in messages:
+        for m in message:
+            learn(dictionary, first_words, m)
+
+    for x in dictionary:
+        print(x)
+        for y in dictionary[x]:
+            print(y, ":", dictionary[x][y])
+
+    # TODO save new dictionary into the database
